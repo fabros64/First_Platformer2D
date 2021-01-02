82aaf6b428ad82f34f233529cd169968821bfbe1
fabros64 <32464588+fabros64@users.noreply.github.com>
2021-01-02 18:11:31 +0100

    Add first enemy and attack functionality

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float attackRange = 0.1f;
+    public int attackDamage = 40;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+
+    void Update()
+    {
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                Attack1();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }       
+    }
+
+    void Attack1()
+    {
+        animator.SetTrigger("Attack1");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
