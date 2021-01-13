9a2a2c37755d29a8f5e9798be982dc0cab55f37d
fabros64 <32464588+fabros64@users.noreply.github.com>
2021-01-13 01:54:12 +0100

    add Camera Shakink effect

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static Game game;
+
+    private void Awake()
+    {
+        if(game == null)
+            game = GameObject.FindGameObjectWithTag("GameController").GetComponent<Game>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
